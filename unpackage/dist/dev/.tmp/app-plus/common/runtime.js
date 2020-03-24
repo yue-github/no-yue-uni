4f8933c83a12fb987ad0b8e7210ac97ffb5c6e2e
yue-github <1079051908@qq.com>
2020-03-24 19:34:47 +0800

    update

--- unpackage/dist/dev/.tmp/app-plus/common/runtime.js
+++ unpackage/dist/dev/.tmp/app-plus/common/runtime.js
@@ -102,11 +102,11 @@
 /******/
 /******/
 /******/ 		// mini-css-extract-plugin CSS loading
-/******/ 		var cssChunks = {"components/pay/appPay":1,"components/uni-tag/uni-tag":1};
+/******/ 		var cssChunks = {"components/pay/appPay":1,"components/pay/miniProgramPay":1,"components/uni-tag/uni-tag":1};
 /******/ 		if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 		else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 			promises.push(installedCssChunks[chunkId] = new Promise(function(resolve, reject) {
-/******/ 				var href = "" + ({"components/pay/appPay":"components/pay/appPay","components/uni-tag/uni-tag":"components/uni-tag/uni-tag"}[chunkId]||chunkId) + ".wxss";
+/******/ 				var href = "" + ({"components/pay/appPay":"components/pay/appPay","components/pay/miniProgramPay":"components/pay/miniProgramPay","components/uni-tag/uni-tag":"components/uni-tag/uni-tag"}[chunkId]||chunkId) + ".wxss";
 /******/ 				var fullhref = __webpack_require__.p + href;
 /******/ 				var existingLinkTags = document.getElementsByTagName("link");
 /******/ 				for(var i = 0; i < existingLinkTags.length; i++) {
